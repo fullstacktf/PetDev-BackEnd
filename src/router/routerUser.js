fc5e3b62ffffb457d4f51a7209b3f1b7230ae6df
Aarón Pérez <aaron@pop-os.localdomain>
2019-07-24 13:21:27 +0100

    Formatting

--- src/router/routerUser.js
+++ src/router/routerUser.js
@@ -7,4 +7,5 @@ router.get('/:id', userController.findById);
 router.post('/', userController.addUser);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
+
 module.exports = router;
