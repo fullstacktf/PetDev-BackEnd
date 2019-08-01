750ecf92a486b4b4953f69fc88f2bdac49d9a0bd
Aarón Pérez <aaron@pop-os.localdomain>
2019-08-01 14:01:16 +0100

    Checked that it works properly, need to learn how the maxDistance field works

--- src/controller/mapController.js
+++ src/controller/mapController.js
@@ -1,21 +1,21 @@
 const User = require('../models/User');
 
 function showUsers(req, res, next) {
-
-	User.aggregate([
-        {
-            $geoNear: {
-                near: [ 11 , 12 ] ,
-                distanceField: "dist.calculated",
-                maxDistance: 1000000000,
-                spherical: true                
-            }
-        }
-    ],
-	function(err, results) {
-console.log(results)	}
-)
+	User.aggregate(
+		[
+			{
+				$geoNear: {
+					near: [-16.315813064575195, 28.48370514774725],
+					distanceField: 'dist.calculated',
+					maxDistance: 0.5,
+					spherical: true
+				}
+			}
+		],
+		function(err, results) {
+			res.json(results);
+		}
+	);
 }
-
 
 module.exports = { showUsers };
