18b21e4b6becd4970349d6721ee03789e53ab182
Aarón Pérez <aaron@pop-os.localdomain>
2019-08-02 11:00:53 +0100

    Done

--- src/controller/mapController.js
+++ src/controller/mapController.js
@@ -1,18 +1,22 @@
 const User = require('../models/User');
 
-function showUsers(req, res, next) {
+function showUsers(req, res) {
 	User.aggregate(
 		[
 			{
 				$geoNear: {
-					near: [-16.315813064575195, 28.48370514774725],
+					near: [parseFloat(req.query.lng), parseFloat(req.query.lat)],
 					distanceField: 'dist.calculated',
-					maxDistance: 0.5,
+					maxDistance: 10,
 					spherical: true
 				}
 			}
 		],
 		function(err, results) {
+			/* res.json(results); */
+			console.log(
+				results.map(home => Math.ceil(home.dist.calculated / 10)) + ' km '
+			);
 			res.json(results);
 		}
 	);
