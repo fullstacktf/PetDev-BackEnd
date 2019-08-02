4c6448d6551048999de9be032ab7dbc9cfa3bdfb
josué <besteujt@gmail.com>
2019-08-02 16:36:51 +0100

    CORS messing around

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 const corsOptions = {
-	origin: 'http://localhost'
+	origin: ['http://localhost:3000', 'http://localhost:3001']
 }
 app.use(cors(corsOptions));
 
