3c57fd8cc7c95518519e9ac07ec0615e7871c562
Aaron Perez <aarperper@gmail.com>
2019-08-06 19:22:54 +0100

    Models reviewed

--- src/models/User.js
+++ src/models/User.js
@@ -1,130 +1,124 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require("bcrypt-nodejs");
 
 const GeoSchema = new Schema({
-	type: {
-		type: String,
-		default: 'Point'
-	},
-	coordinates: {
-		type: [Number],
-		index: '2dsphere'
-	}
+  type: {
+    type: String,
+    default: "Point"
+  },
+  coordinates: {
+    type: [Number],
+    index: "2dsphere"
+  }
 });
 
 const UserSchema = new Schema({
-	name: {
-		type: String
-	},
-	lastName: {
-		type: String
-	},
-	userName: {
-		type: String
-	},
-	houseType: {
-		type: String
-	},
-	email: {
-		type: String
-	},
-	password: {
-		type: String
-	},
-	birthDate: {
-		type: Date
-	},
-	avatarURL: {
-		type: String
-	},
-	description: {
-		type: String
-	},
-	petPreferences: {
-		type: [String]
-	},
-	coins: {
-		type: Number
-	},
-	rating: {
-		type: Number
-	},
-	createdAt: {
-		type: Date
-	},
-	updateAt: {
-		type: Date
-	},
-	messages: [
-		{
-			text: {
-				type: String
-			},
-			fromUserID: {
-				type: String
-			},
-			sentDate: {
-				type: Date
-			},
-			readMessage: {
-				type: Boolean
-			},
-			bookingID: {
-				type: String
-			}
-		}
-	],
+  name: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  userName: {
+    type: String
+  },
+  houseType: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  password: {
+    type: String
+  },
+  birthDate: {
+    type: Date
+  },
+  avatarURL: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  petPreferences: {
+    type: [String]
+  },
+  coins: {
+    type: Number
+  },
+  rating: {
+    type: Number
+  },
+  createdAt: {
+    type: Date
+  },
+  updateAt: {
+    type: Date
+  },
+  messages: [
+    {
+      text: {
+        type: String
+      },
+      fromUserID: {
+        type: String
+      },
+      sentDate: {
+        type: Date
+      },
+      readMessage: {
+        type: Boolean
+      },
+      bookingID: {
+        type: String
+      }
+    }
+  ],
 
-	geo: GeoSchema,
+  geo: GeoSchema,
 
-	address: {
-		country: {
-			type: String
-		},
-		province: {
-			type: String
-		},
-		addressLine: {
-			type: String
-		},
-		postal: {
-			type: String
-		}
-	},
-	pets: [
-		{
-			chip: {
-				type: String
-			},
-			name: {
-				type: String
-			},
-			birthDate: {
-				type: Date
-			},
-			energy: {
-				type: String
-			},
-			specie: {
-				type: String
-			},
-			description: {
-				type: String
-			},
-			picturesURL: {
-				type: [String]
-			}
-		}
-	]
+  address: {
+    country: {
+      type: String
+    },
+    province: {
+      type: String
+    },
+    addressLine: {
+      type: String
+    },
+    postal: {
+      type: String
+    }
+  },
+  pets: [
+    {
+      chip: {
+        type: String
+      },
+      petName: {
+        type: String
+      },
+      specie: {
+        type: String
+      },
+      petDescription: {
+        type: String
+      },
+      picturesURL: {
+        type: String
+      }
+    }
+  ]
 });
 
 UserSchema.methods.encryptPassword = password => {
-	return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 };
 
 UserSchema.methods.comparePassword = function(password) {
-	return bcrypt.compareSync(password, this.password);
+  return bcrypt.compareSync(password, this.password);
 };
 
-module.exports = mongoose.model('users', UserSchema);
+module.exports = mongoose.model("users", UserSchema);
