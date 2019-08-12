2b642f697064f8d790af6731fe2a35401316ca23
Alejandro <alesetneuf@gmail.com>
2019-08-12 23:32:51 +0100

    Restored

--- src/controller/mapController.js
+++ src/controller/mapController.js
@@ -1,27 +1,22 @@
 const User = require('../models/User');
 
-async function showUsers(req, res) {
+function showUsers(req, res) {
 	const { lng, lat, maxDist } = req.query;
-	const nearUsers = await User.find({
-		
-				location: {
-					$near: {
-					$maxDistance: 1000,
-					$geometry: {
-					 type: "Point",
-					 coordinates: [lng, lat]
-					}
-					/* near: [+lng, +lat],
+	User.aggregate(
+		[
+			{
+				$geoNear: {
+					near: [+lng, +lat],
 					distanceField: 'dist.calculated',
 					maxDistance: +maxDist,
-					spherical: true */
-				},
-			}},
-	
+					spherical: true
+				}
+			}
+		],
 
 		function(err, results) {
 			results.map(
-				user => (user.dist.calculated = user.dist.calculated/*  * 6.371 */)
+				user => (user.dist.calculated = user.dist.calculated * 6.371)
 			); // Distance from radians to kms
 
 			res.json(results);
